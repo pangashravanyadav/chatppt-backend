4112a767e1952ed5dc2b4dceb0dca63718a6517d
pangashravanyadav <105449771+pangashravanyadav@users.noreply.github.com>
2026-03-31 12:54:51 +0530

    Add new allowed origin for CORS

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 // 5. Middleware: allows frontend (different port) to call this server
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://chat-qvb7pk3dy-pangashravanyadavs-projects.vercel.app","https://chatppt-frontend-flame.vercel.app/",
+  "https://chat-qvb7pk3dy-pangashravanyadavs-projects.vercel.app","https://chatppt-frontend-flame.vercel.app/","https://chatppt-frontend-pangashravanyadavs-projects.vercel.app/"
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
